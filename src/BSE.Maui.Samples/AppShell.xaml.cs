1b6b906bd66e2e331eb72ab34c12f8792ec286a3
Uwe Eichkorn <git.bsetunes@outlook.com>
2024-03-28 09:11:40 +0100

    initial commit

---
+++ src/BSE.Maui.Samples/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BSE.Maui.Samples
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/BSE.Maui.Samples/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BSE.Maui.Samples
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
